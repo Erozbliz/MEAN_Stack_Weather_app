84efb90776ef0c3b6ed37ffb808db51b3ffe7ed9
Erozbliz <erozbliz@hotmail.com>
2016-12-05 13:15:32 -0500

    Possibilité de créer un user
    Controlleur + ihm ok

--- TEST3/client/app.js
+++ TEST3/client/app.js
@@ -21,6 +21,22 @@ myApp.config(function($routeProvider){
 		controller:'BooksController',
 		templateUrl: 'views/edit_book.html'
 	})
+	.when('/users', {
+		controller:'UsersController',
+		templateUrl: 'views/users.html'
+	})
+	.when('/users/details/:id',{
+		controller:'UsersController',
+		templateUrl: 'views/user_details.html'
+	})
+	.when('/users/add',{
+		controller:'UsersController',
+		templateUrl: 'views/add_user.html'
+	})
+	.when('/users/edit/:id',{
+		controller:'UsersController',
+		templateUrl: 'views/edit_user.html'
+	})
 	.otherwise({
 		redirectTo: '/'
 	});
