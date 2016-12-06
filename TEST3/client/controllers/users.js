0bffee7f17d7e535ce106f319d1a81c8b66ee0db
Erozbliz <erozbliz@hotmail.com>
2016-12-06 13:27:49 -0500

    Ajout météo ok

--- TEST3/client/controllers/users.js
+++ TEST3/client/controllers/users.js
@@ -36,4 +36,58 @@ myApp.controller('UsersController', ['$scope', '$http', '$location', '$routePara
 			window.location.href='#/users';
 		});
 	}
+
+	//Obtenir la météo d'une seule ville
+	 $scope.getWeather = function() {
+      $http.get('http://api.openweathermap.org/data/2.5/weather?q=Sherbrooke&APPID=83512f0ca80b87807f61db32870c85d7&units=metric')
+        .success(function(data) {
+          console.log(data);
+          $scope.name = data.name;
+          $scope.main = data.main;
+          $scope.wind = data.wind;
+          $scope.clouds = data.clouds;
+        })
+        .error(function(data) {
+          console.log('error: ' + data);
+        });
+
+    };
+
+    $scope.getAllWeather = function() {
+    	listCity = ['Quebec', "Montreal"];
+		alldataweather = [];
+		for (i = 0; i < listCity.length; i++) {
+		    $http.get('http://api.openweathermap.org/data/2.5/weather?q='+listCity[i]+'&APPID=83512f0ca80b87807f61db32870c85d7&units=metric')
+	        .success(function(data) {
+	          //console.log(data);
+	          alldataweather.push(data);
+	        })
+	        .error(function(data) {
+	          console.log('error: ' + data);
+	        });
+		}
+		//ajout dans le scope
+	 	$scope.alldataweathers = alldataweather;
+		//console.log(alldataweather);
+    };
+
+
+    $scope.search = function() {
+    	var myCity = angular.copy($scope.searchCity);
+        console.log('search ' + myCity);
+        $http.get('http://api.openweathermap.org/data/2.5/weather?q='+myCity+'&APPID=83512f0ca80b87807f61db32870c85d7&units=metric')
+        .success(function(data) {
+          console.log(data);
+          $scope.name = data.name;
+          $scope.main = data.main;
+          $scope.wind = data.wind;
+          $scope.clouds = data.clouds;
+        })
+        .error(function(data) {
+          console.log('error: ' + data);
+        });
+    };
+    //$scope.search();
+
+	
 }]);
